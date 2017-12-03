8e6a9fff9c50d16b8fee7542c0cb61fdb7096211
haohailiang <646263409@qq.com>
2017-12-03 16:55:12 +0800

    fontmin代码片段

---
+++ README.md
@@ -0,0 +1,14 @@
+# fontmin-demo
+鲨鱼级神器, 可以把任何ttf文件转化成压缩版的eot\svg\ttf\woff等web字体
+# 安装
+```
+npm install fontmin --save
+npm install gulp-rename --save
+```
+# 字体文件获取
+控制面板 --> 小图标 --> 字体 --> 搜索 '微软雅黑' --> copy出.ttf文件到fonts目录
+# 使用
+```
+# 修改entry.js文件
+node entry.js
+```
---
+++ entry.js
@@ -0,0 +1,27 @@
+var Fontmin = require('fontmin');
+
+var srcPath = 'fonts/*.ttf'; // 字体源文件
+var destPath = 'asset/fonts';    // 输出路径
+var text = '我说你是人间的四月天；笑响点亮了四面风；轻灵在春的光艳中交舞着变。';
+
+// 初始化
+var fontmin = new Fontmin()
+    .src(srcPath)               // 输入配置
+    .use(Fontmin.glyph({        // 字型提取插件
+        text: text              // 所需文字
+    }))
+    .use(Fontmin.ttf2eot())     // eot 转换插件a
+    .use(Fontmin.ttf2woff())    // woff 转换插件a
+    .use(Fontmin.ttf2svg())     // svg 转换插件
+    .use(Fontmin.css())         // css 生成插件
+    .dest(destPath);            // 输出配置
+
+// 执行
+fontmin.run(function (err, files, stream) {
+
+    if (err) {                  // 异常捕捉
+        console.error(err);
+    }
+
+    console.log('字体压缩完成');        // 成功
+});
